bf22b1497e6715b5c12bc2e62f2d2d3cc5b18ae0
Asugamy <93414564+Asugamy@users.noreply.github.com>
2022-08-11 01:16:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -16,36 +16,36 @@ col1, col2, col3 = st.columns(3)
 
 with col1:
     with st.form("Roxo Verde"):
-        st.markdown("## Roxo Verde")
+        st.markdown("### Roxo Verde")
         st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80")
         st.form_submit_button('Mais')
 
 with col2:
     with st.form("Lagoinha"):
-        st.markdown("## Lagoinha")
+        st.markdown("### Lagoinha")
         st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80")
         st.form_submit_button('Mais')
 
 with col3:
     with st.form("Jd Primavera"):
-        st.markdown("## Jd Primavera")
+        st.markdown("### Jd Primavera")
         st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80")
         st.form_submit_button('Mais')
 
 with col1:
     with st.form("b"):
-        st.markdown("## Roxo Verde")
+        st.markdown("### Roxo Verde")
         st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80")
         st.form_submit_button('Mais')
 
 with col2:
     with st.form("a"):
-        st.markdown("## Lagoinha")
+        st.markdown("### Lagoinha")
         st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80")
         st.form_submit_button('Mais')
 
 with col3:
     with st.form("c"):
-        st.markdown("## Jd Primavera")
+        st.markdown("### Jd Primavera")
         st.image("https://images.unsplash.com/photo-1548407260-da850faa41e3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1487&q=80")
         st.form_submit_button('Mais')
